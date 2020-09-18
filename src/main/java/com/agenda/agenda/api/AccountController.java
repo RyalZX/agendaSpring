afc83df2a87df86a4d52836edf7376be90a9560d
Gianluca Lauro <giangi.lauro@gmail.com>
2020-09-18 21:01:23 +0200

    Updated CrossOrigin

--- src/main/java/com/agenda/agenda/api/AccountController.java
+++ src/main/java/com/agenda/agenda/api/AccountController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "agendaspring.herokuapp.com")
 @RequestMapping("/account")
 @RestController
 public class AccountController {
